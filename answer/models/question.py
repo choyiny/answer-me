eb3219c9afa6780fc2bc03c34d79109ccbd9ad09
Daniel Xu <niellxu@yahoo.ca>
2018-11-21 17:02:05 -0500

    Added new table, fixed bugs

--- answer/models/question.py
+++ answer/models/question.py
@@ -36,3 +36,37 @@ class Question(db.Model):
             'question': self.question,
             'answers': possible_answers
         }
+
+
+class QuickQuestion(db.Model):
+    __tablename__ = "quick_questions"
+
+    question_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+    question = db.Column(db.Text, nullable=False)
+
+    correct_answer = db.Column(db.Text, nullable=False)
+    wrong_answer1 = db.Column(db.Text, nullable=False)
+    wrong_answer2 = db.Column(db.Text, nullable=False)
+    wrong_answer3 = db.Column(db.Text, nullable=False)
+
+    asked = db.Column(db.Boolean, default=False)
+
+    def __init__(self, question, correct, wrong1, wrong2, wrong3):
+        self.question = question
+        self.correct_answer = correct
+        self.wrong_answer1 = wrong1
+        self.wrong_answer2 = wrong2
+        self.wrong_answer3 = wrong3
+        self.asked = False
+
+    def __repr__(self):
+        return f"<QuickQuestion {self.question_id}>"
+
+    def get_dict(self):
+        possible_answers = [self.correct_answer, self.wrong_answer1, self.wrong_answer2, self.wrong_answer3]
+        random.shuffle(possible_answers)
+        return {
+            'question': self.question,
+            'answers': possible_answers
+        }
--- postgres/create.sql
+++ postgres/create.sql
@@ -18,3 +18,14 @@ CREATE TABLE players (
         PRIMARY KEY (player_id),
         UNIQUE (nickname)
 );
+
+CREATE TABLE quick_questions (
+        question_id SERIAL NOT NULL, 
+        question TEXT NOT NULL, 
+        correct_answer TEXT NOT NULL, 
+        wrong_answer1 TEXT NOT NULL, 
+        wrong_answer2 TEXT NOT NULL, 
+        wrong_answer3 TEXT NOT NULL, 
+        asked BOOLEAN, 
+        PRIMARY KEY (question_id)
+)
